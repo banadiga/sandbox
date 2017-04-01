bb386294c35dadc70f6afa7da597dd3fdd0ef2e9
Ihor Banadiga <banadiga@gmail.com>
2017-04-01 17:22:48 +0300

    - try to run gradle module as maven module

---
+++ groovymodule/src/test/groovy/com/banadiga/groovymodule/DemoApplicationTests.groovy
@@ -0,0 +1,18 @@
+package com.banadiga.groovymodule
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+    Assert.assertEquals(1, 1);
+  }
+
+}
